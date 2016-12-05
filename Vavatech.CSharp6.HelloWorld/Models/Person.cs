dbf40988d7089ced4474d8e1735a74257f0b0820
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-12-05 12:59:24 +0100

    Utworzenie właściwości

--- Vavatech.CSharp6.HelloWorld/Models/Person.cs
+++ Vavatech.CSharp6.HelloWorld/Models/Person.cs
@@ -14,13 +14,44 @@ namespace Vavatech.CSharp6.HelloWorld.Models
 
         public double Weight;
 
-        public short Age;
+        private short _Age;
+
+        public short Age
+        {
+            get
+            {
+                return _Age;
+            }
+
+            set
+            {
+                if ((value > 0) && (value < 122))
+                {
+                   _Age = value;
+                }
+            }
+        }
+
+        //public short GetAge()
+        //{
+        //    return this.Age;
+        //}
+
+        //public void SetAge(short value)
+        //{
+        //    if ((value > 0) && (value < 122))
+        //    { 
+        //        this.Age = value;
+        //    }
+        //}
+
+      
 
         public string FullName 
         {
             get
             {
-                return $"{FirstName} {LastName}";
+                return $"{FirstName} {LastName} {Age}";
             }
         }
             
