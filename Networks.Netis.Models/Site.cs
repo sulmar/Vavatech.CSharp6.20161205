58eeb47888d7c2f2788ea5ab327bb670799d7b60
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-12-06 10:28:33 +0100

    Utworzenie modelu danych

---
+++ Networks.Netis.Models/Site.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networks.Netis.Models
+{
+    public abstract class Site : Base
+    {
+        public int SiteId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string MachineName { get; set; }
+    }
+}
