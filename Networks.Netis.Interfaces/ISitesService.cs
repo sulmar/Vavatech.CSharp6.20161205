1a3a002cab6225ca20d5b534e1151caa8b3142a9
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-12-06 10:57:18 +0100

    Utworzenie implementacji mock

---
+++ Networks.Netis.Interfaces/ISitesService.cs
@@ -0,0 +1,25 @@
+﻿using Networks.Netis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networks.Netis.Interfaces
+{
+    public interface ISitesService
+    {
+
+        List<Site> Get();
+
+        Site Get(string code);
+
+        void Add(Site site);
+
+        void Update(Site site);
+
+        void Remove(int siteId);
+
+
+    }
+}
---
+++ Networks.Netis.MockServices/MockSitesService.cs
@@ -0,0 +1,53 @@
+﻿using Networks.Netis.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Networks.Netis.Models;
+
+namespace Networks.Netis.MockServices
+{
+    public class MockSitesService : ISitesService
+    {
+        private List<Site> _Sites = new List<Site>
+        {
+            new Site2G { SiteId = 1, Code = "1100", Name = "Olesińska 2G", MachineName = "BTS 1100" },
+            new Site2G { SiteId = 2, Code = "1200", Name = "Dworzec Centralny 2G", MachineName = "BTS 1200" },
+            new Site2G { SiteId = 3, Code = "1400", Name = "Pałac Kultury", MachineName = "BTS 1400" },
+        };
+
+        public void Add(Site site)
+        {
+            _Sites.Add(site);
+        }
+
+        public List<Site> Get()
+        {
+            return _Sites;
+        }
+
+        public Site Get(string code)
+        {
+            foreach (var site in _Sites)
+            {
+                if (site.Code == code)
+                {
+                    return site;
+                }
+            }
+
+            return null;
+        }
+
+        public void Remove(int siteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Site site)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
