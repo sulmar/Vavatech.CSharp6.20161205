eb42df9c936fe66431f7d6ceaca2a783d138c052
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-12-06 12:37:03 +0100

    Utworzenie implementacji Xml Web Service

---
+++ Networks.Netis.XmlWebServiceClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Networks.Netis.Interfaces;
+using Networks.Netis.XmlWebServiceClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networks.Netis.XmlWebServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetSitesTest();
+        }
+
+        private static void GetSitesTest()
+        {
+            ISitesService sitesService = new XmlWebServiceSitesService();
+
+            var sites = sitesService.Get();
+
+            foreach (var site in sites)
+            {
+                Console.WriteLine(site);
+            }
+        }
+    }
+}
---
+++ Networks.Netis.XmlWebServiceClient/Services/XmlWebServiceSitesService.cs
@@ -0,0 +1,38 @@
+﻿using Networks.Netis.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Networks.Netis.Models;
+
+namespace Networks.Netis.XmlWebServiceClient.Services
+{
+    class XmlWebServiceSitesService : ISitesService
+    {
+        public void Add(Site site)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Site> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Site Get(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int siteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Site site)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
